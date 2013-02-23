c488cd582116c23d3c8f198be334d93e9b12e5f7
marc <marc@tsunami.site>
2013-02-23 12:10:33 -0800

    initial bulk checkin

---
+++ app.js
@@ -0,0 +1,175 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , contact = require('./routes/contact')
+  , home = require('./routes/home')
+  , about = require('./routes/about')
+  , http = require('http')
+  , path = require('path')
+  , flash = require('connect-flash')
+  , passport = require('passport')
+  , util = require('util')
+  , LocalStrategy = require('passport-local').Strategy;
+
+var app = express();
+
+
+var users = [
+    { id: 1, username: 'bob', password: 'secret', email: 'bob@example.com' }
+    , { id: 2, username: 'joe', password: 'birthday', email: 'joe@example.com' }
+    , { id: 3, username: 'marc', password: '40niner', email: 'marc@fogtest.com' }
+];
+
+
+function findById(id, fn) {
+    var idx = id - 1;
+    if (users[idx]) {
+        fn(null, users[idx]);
+    } else {
+        fn(new Error('User ' + id + ' does not exist'));
+    }
+}
+
+function findByUsername(username, fn) {
+    for (var i = 0, len = users.length; i < len; i++) {
+        var user = users[i];
+        if (user.username === username) {
+            return fn(null, user);
+        }
+    }
+    return fn(null, null);
+}
+
+
+// Passport session setup.
+// To support persistent login sessions, Passport needs to be able to
+// serialize users into and deserialize users out of the session. Typically,
+// this will be as simple as storing the user ID when serializing, and finding
+// the user by ID when deserializing.
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+    findById(id, function (err, user) {
+        done(err, user);
+    });
+});
+
+
+// Use the LocalStrategy within Passport.
+// Strategies in passport require a `verify` function, which accept
+// credentials (in this case, a username and password), and invoke a callback
+// with a user object. In the real world, this would query a database;
+// however, in this example we are using a baked-in set of users.
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+        // asynchronous verification, for effect...
+        process.nextTick(function () {
+
+            // Find the user by username. If there is no user with the given
+            // username, or the password is not correct, set the user to `false` to
+            // indicate failure and set a flash message. Otherwise, return the
+            // authenticated `user`.
+            findByUsername(username, function(err, user) {
+                if (err) { return done(err); }
+                if (!user) { return done(null, false, { message: 'Unknown user ' + username }); }
+                if (user.password != password) { return done(null, false, { message: 'Invalid password' }); }
+                return done(null, user);
+            })
+        });
+    }
+));
+
+
+
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser('your secret here'));
+  app.use(express.session());
+  // Use connect-flash middleware. This will add a `req.flash()` function to
+  // all requests, matching the functionality offered in Express 2.x.
+
+  app.use(flash());
+  // Initialize Passport! Also use passport.session() middleware, to support
+    // persistent login sessions (recommended).
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(app.router);
+  app.use(require('stylus').middleware({ src: __dirname + '/public' }));
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure(function(){
+    app.use(app.router);
+});
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/users', user.list);
+
+app.get('/', home.show);
+app.get('/about', about.show);
+
+app.get('/contact', contact.show);
+
+
+app.get('/login', function(req, res){
+    req.session.messages = ['foo']
+    res.render('login', { user: req.user, message: req.flash('error'),title: 'Login' });
+});
+
+// POST /login
+// Use passport.authenticate() as route middleware to authenticate the
+// request. If authentication fails, the user will be redirected back to the
+// login page. Otherwise, the primary route function function will be called,
+// which, in this example, will redirect the user to the home page.
+//
+// curl -v -d "username=bob&password=secret" http://127.0.0.1:3000/login
+app.post('/login',
+    passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }),
+    function(req, res) {
+        res.redirect('/');
+    });
+
+// POST /login
+// This is an alternative implementation that uses a custom callback to
+// acheive the same functionality.
+/*
+ app.post('/login', function(req, res, next) {
+ passport.authenticate('local', function(err, user, info) {
+ if (err) { return next(err) }
+ if (!user) {
+ req.flash('error', info.message);
+ return res.redirect('/login')
+ }
+ req.logIn(user, function(err) {
+ if (err) { return next(err); }
+ return res.redirect('/users/' + user.username);
+ });
+ })(req, res, next);
+ });
+ */
+
+app.get('/logout', function(req, res){
+    req.logout();
+    res.redirect('/');
+});
+
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
---
+++ routes/contact.js
@@ -0,0 +1,6 @@
+
+exports.show = function(req, res){
+    res.render('contact', {
+        title: 'Contact'
+    });
+};
